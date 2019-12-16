5ed06903ab25acf5160aba9f057ed079d58f4494
tameer7595 <53409555+tameer7595@users.noreply.github.com>
2019-12-16 22:54:15 +0200

    Add files via upload
    get parent data and send to the server

---
+++ front_end/BabySaver.py
@@ -0,0 +1,25 @@
+import requests
+from flask import Flask, escape, url_for, request, render_template
+
+app = Flask(__name__)
+addr = 'http://10.144.71.51:5000'
+func_url = addr + '/api/web_page'
+
+@app.route('/')
+def index():
+    return render_template('parentData.html')
+
+@app.route('/', methods = ['POST'])
+def getData():
+    data = []
+    data.append(request.form['CAR NUMBER'])
+    data.append(request.form['PHONE NUMBER'])
+    data_string = ', '.join(data)
+    response = requests.post(func_url, data=data_string)
+
+    return data_string
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
